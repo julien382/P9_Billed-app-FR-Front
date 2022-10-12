326f34355ef7cdae5e986b97531c228fb5776789
julien382 <julhermain@gmail.com>
2022-10-12 23:29:47 +0200

    bills

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -20,7 +20,13 @@ const row = (bill) => {
   }
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+  // trier data par date
+  const dataSorted = data.sort((a, b) => {
+    const dateA = new Date(a.date)
+    const dateB = new Date(b.date)
+    return dateB - dateA
+  })
+  return (dataSorted && dataSorted.length) ? dataSorted.map(bill => row(bill)).join("") : ""
 }
 
 export default ({ data: bills, loading, error }) => {

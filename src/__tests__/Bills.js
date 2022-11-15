bdf51510c4b4d256d8afb548f3ab4280fa84bb96
julien382 <julhermain@gmail.com>
2022-11-15 17:06:42 +0100

    85%

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,13 +2,16 @@
  * @jest-environment jsdom
  */
 
- import { screen, waitFor } from "@testing-library/dom"
- import BillsUI from "../views/BillsUI.js"
- import { bills } from "../fixtures/bills.js"
- import { ROUTES_PATH } from "../constants/routes.js";
+ import { screen, waitFor, fireEvent } from "@testing-library/dom";
+ import BillsUI from "../views/BillsUI.js";
+ import { bills } from "../fixtures/bills.js";
+ import {  ROUTES, ROUTES_PATH } from "../constants/routes.js";
  import { localStorageMock } from "../__mocks__/localStorage.js";
+ import Bills from "../containers/Bills.js";
+ import mockStore from "../__mocks__/store";
+ 
  import router from "../app/Router.js";
- import mockedStore from "../__mocks__/store.js" 
+ jest.mock("../app/Store", () => mockStore)
 
  describe("Given I am connected as an employee", () => {
    describe("When I am on Bills Page", () => {
@@ -34,6 +37,7 @@
        expect(mailIcon.classList.contains('active-icon')).not.toBeTruthy()
  
      })
+
      test("Then bills should be ordered from earliest to latest", async () => {
        document.body.innerHTML = BillsUI({ data: bills })
        const dates = screen.getAllByText(/^[0-9]{2}\s\w{3}\.\s[0-9]{2}$/gm).map(a => a.innerHTML)
@@ -41,27 +45,37 @@
        const datesSorted = [...dates].sort(antiChrono)
        expect(dates).toEqual(datesSorted)
      })
-/*
-     test("Then bills ", async () => {
-       const pageBill = new PageBill({xxx, zzz, yyy})
-       const handleShowTickets1 = jest.fn((e) => pageBill.handleClickNewBill())
-   
-       const icon1 = screen.getByTestId('arrow-icon1')
-   
-       icon1.addEventListener('click', handleShowTickets1)
-   
-       userEvent.click(icon1)
-   
-       expect(handleShowTickets1).toHaveBeenCalled()
-      
-    })
-
-    test("Then the method getBill should return bills", () => {
-      const bill = new Bills({document, onNavigate: null, store: mockedStore, localStorage: null})
-      return bill.getBills().then((data) => {
-        expect(data).toEqual(bills)
-      })
-    })*/
-
    })
- }) 
+ });
+
+ describe("when I click on the IconEye", () => {//Lorsque je clique sur l'icône œil
+  test("Then modal should open", () => {//Ensuite, la modale devrait s'ouvrir
+    Object.defineProperty(window, localStorage, { value: localStorageMock });//simule des données dans le localstorage
+    window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }));//on simule en utilisateur connécter de type employé
+    document.body.innerHTML = BillsUI({ data: bills })//création de la constante la modale facture de l'employé
+
+    const onNavigate = (pathname) => {//navigation vers la route bills
+      document.body.innerHTML = ROUTES({ pathname });
+    };
+
+    const billsContainer = new Bills({//creation d'une facture
+      document,
+      onNavigate,
+      localStorage: localStorageMock,
+      store: null,
+    });
+
+    //MOCK de la modale
+    $.fn.modal = jest.fn();//affichage de la modale
+
+    //MOCK L'ICÔNE DE CLIC
+    const handleClickIconEye = jest.fn(() => {//fonction qui simule un click
+      billsContainer.handleClickIconEye;
+    });
+    const firstEyeIcon = screen.getAllByTestId("icon-eye")[0];
+    firstEyeIcon.addEventListener("click", handleClickIconEye);//surveil un événement au click sur l'oeil
+    fireEvent.click(firstEyeIcon);//click sur l'icone
+    expect(handleClickIconEye).toHaveBeenCalled();//vérifie si l'evenement au click a été appeler
+    expect($.fn.modal).toHaveBeenCalled();// vérifie si la modale est appeler
+  });
+});
